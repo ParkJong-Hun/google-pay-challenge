dc27ceffedebaf17686798870c65f4905264c8b2
Park JongHun <thesn515@gmail.com>
2025-01-25 17:12:22 +0900

    fix: target sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "co.kr.parkjonghun.google_pay_challenge"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "co.kr.parkjonghun.google_pay_challenge"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
